c091a3ae611fc0612b5e1b92270437c425d7ce7d
Narayandwivedi <desinplus1@gmail.com>
2025-07-05 13:05:25 +0530

    change backend url

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,9 +4,9 @@ import axios from "axios"
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-    const BACKEND_URL = `http://localhost:8080`;
+    // const BACKEND_URL = `http://localhost:8080`;
     // const BACKEND_URL = `http://168.231.120.131:8080`;
-    //   const BACKEND_URL = `https://api.winnersclubs.fun`;
+      const BACKEND_URL = `https://api.winnersclubs.fun`;
      const [stats , setStats] = useState('')
 
      const fetchStats = async()=>{
