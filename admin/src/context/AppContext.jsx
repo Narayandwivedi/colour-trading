3f48b6ccb5ad224fae01f39397b4796ed96dab45
Narayandwivedi <desinplus1@gmail.com>
2026-05-27 19:10:52 +0530

    improve ui

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const BACKEND_URL = `http://localhost:8080`; 
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080'; 
 
   const [stats, setStats] = useState("");
   const [userData, setUserData] = useState(null);
